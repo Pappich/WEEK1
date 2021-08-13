98aff2391c762a0232b53b4c36180df9af974f9b
Pappich Pongpattanawut <88368170+Pappich@users.noreply.github.com>
2021-08-13 15:28:45 +0700

    add new files
    add a bunch of new files

---
+++ w1_2.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,i=1,j=1,k;
+    printf("Enter size : ");
+    scanf("%d",&n);
+    i=n*n;
+    k=i;
+    while(i!=0)
+    {
+        if((i-1)%n!=0 || i==k)
+        {
+            printf("*");
+            i-=1;
+        }
+        else if((i-1)%n==0 )
+        {
+            printf("*\n");
+            i -= 1;
+        }
+    }
+    return 0;    
+}
